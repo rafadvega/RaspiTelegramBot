bdfdfaeef804a6e54813ce916b2d42099c8c65ab
Rafael de Vega Flores <rvega@gradiant.org>
2017-09-05 17:23:47 +0200

    Añadido almacenaje de facturas

--- py-telegram-bot.py
+++ py-telegram-bot.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 # Descripción: Bot de Telegram para uso personal
-# Autor: Rafael de Vega @rdevega
+# Autor: Rafael de Vega
 
 import telebot
 import os
 import definitions
 from telebot import types
+import sqlite3
+
+okHand = u'\U0001F44D'
 
 bot = telebot.TeleBot(definitions.token)
 users = list()
@@ -32,7 +35,7 @@ def command_hola(m):
     bot.send_message(chat_id, definitions.hello + name)
 
 @bot.message_handler(commands=['startvnc'])
-def command_prueba(m):
+def command_startvnc(m):
     chat_id = m.chat.id
     if  chat_id == definitions.userTop or  chat_id == definitions.groupTop:
         name = m.from_user.first_name
@@ -42,7 +45,7 @@ def command_prueba(m):
         bot.send_message(chat_id, definitions.permission_denied)
 
 @bot.message_handler(commands=['plex'])
-def command_prueba(m):
+def command_plex(m):
     chat_id = m.chat.id
     if  chat_id == definitions.userTop or  chat_id == definitions.groupTop:
         name = m.from_user.first_name
@@ -65,7 +68,7 @@ def command_prueba(m):
         bot.send_message(chat_id, definitions.permission_denied)
 
 @bot.message_handler(commands=['samba'])
-def command_prueba(m):
+def command_samba(m):
     chat_id = m.chat.id
     if  chat_id == definitions.userTop or  chat_id == definitions.groupTop:
         name = m.from_user.first_name
@@ -86,5 +89,46 @@ def command_prueba(m):
             bot.send_message(chat_id, 'Debes enviar un comando para Samba (start, stop, restart)')
     else:
         bot.send_message(chat_id, definitions.permission_denied)
+
+@bot.message_handler(commands=['receipt'])
+def command_receipt(m):
+    chat_id = m.chat.id
+    if  chat_id == definitions.userTop or  chat_id == definitions.groupTop:
+        name = m.from_user.first_name
+        text = m.text.split()
+        option = ''
+        if len(text) > 1:
+            option = text[1]
+    else:
+        bot.send_message(chat_id, definitions.permission_denied)
+
+# to create database:
+#con = sqlite3.connect('receipts.db')
+#cursor = con.cursor()
+#cursor.execute("CREATE TABLE facturas (concepto TEXT, fecha TEXT , cantidad NUMERIC)")
+@bot.message_handler(commands=['store'])
+def command_store(m):
+    chat_id = m.chat.id
+    if  chat_id == definitions.userTop or  chat_id == definitions.groupTop:
+        text = m.text.split()
+        if len(text) != 4:
+                bot.send_message(chat_id, 'Formato no válido')
+        else:
+            try:
+                concepto = text[1]
+                fecha = text[2]
+                cantidad = float(text[3])
+                con = sqlite3.connect('receipts.db')
+                cursor = con.cursor()
+                cursor.execute("INSERT INTO  facturas (concepto ,fecha ,cantidad) VALUES (?,?,?)",(concepto, fecha, cantidad))
+                con.commit()
+                cursor.close()
+                con.close()
+                bot.send_message(chat_id, 'Almacenado! '+ okHand)
+            except ValueError:
+                bot.send_message(chat_id, 'Revisa el formato de la cantidad. Recuerda tiene que ser un número, y si tiene decimales sepáralo con un punto')
+    else:
+        bot.send_message(chat_id, definitions.permission_denied)
+
 
 bot.polling(none_stop = True)
